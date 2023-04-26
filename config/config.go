33b5436fabe217c5c2c9ced067777b92f85ed521
fzTelU <fazaalexander@student.telkomuniversity.ac.id>
2023-04-26 12:54:22 +0700

    adding .env file and docker compose for production

--- config/config.go
+++ config/config.go
@@ -2,9 +2,12 @@ package config
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/fazaalexander/go-gorm/model"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -27,40 +30,23 @@ type Config struct {
 }
 
 func InitDB() {
-
-	// erro := godotenv.Load()
-	// if erro != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
-
-	// db_username := os.Getenv("DB_Username")
-	// db_password := os.Getenv("DB_Password")
-	// db_port := os.Getenv("DB_Port")
-	// db_host := os.Getenv("DB_Host")
-	// db_name := os.Getenv("DB_Name")
-
-	// connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-	// 	db_username,
-	// 	db_password,
-	// 	db_host,
-	// 	db_port,
-	// 	db_name,
-	// )
-
-	config := map[string]string{
-		"DB_Username": "root",
-		"DB_Password": "",
-		"DB_Port":     "3306",
-		"DB_Host":     "db",
-		"DB_Name":     "crud_go",
+	erro := godotenv.Load()
+	if erro != nil {
+		log.Fatal("Error loading .env file")
 	}
 
+	db_username := os.Getenv("DB_Username")
+	db_password := os.Getenv("DB_Password")
+	db_port := os.Getenv("DB_Port")
+	db_host := os.Getenv("DB_Host")
+	db_name := os.Getenv("DB_Name")
+
 	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-		config["DB_Username"],
-		config["DB_Password"],
-		config["DB_Host"],
-		config["DB_Port"],
-		config["DB_Name"],
+		db_username,
+		db_password,
+		db_host,
+		db_port,
+		db_name,
 	)
 
 	var err error
